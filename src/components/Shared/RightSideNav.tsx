0aa59b2907f6e4e1eb274b878777307191115027
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-10-04 03:50:22 +0600

    a

--- src/components/Shared/RightSideNav.tsx
+++ src/components/Shared/RightSideNav.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const RightSideNav: React.FC = () => {
-  return <div className="my-10"></div>;
+  return (
+    <div className="my-10">
+      <div className="my-10">Featured News here</div>
+      <div className="mt-52">Advertisement here</div>
+    </div>
+  );
 };
 
 export default RightSideNav;
